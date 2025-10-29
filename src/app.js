fa68b3742d09a129f16860be1ec0417b9ab391f1
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2025-10-29 23:57:08 +0000

    [This is my Card Random Generator for a good poker night! Good luck!]

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  let suits = ["♦", "♥", "♠", "♣"];
+  let suitClasses = ["diamond", "heart", "spade", "club"];
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+  let suitIndex = Math.floor(Math.random() * suits.length);
+  let valueIndex = Math.floor(Math.random() * values.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let suit = suits[suitIndex];
+  let value = values[valueIndex];
+  let suitClass = suitClasses[suitIndex];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.getElementById("card");
+  let topSuit = document.getElementById("top-suit");
+  let bottomSuit = document.getElementById("bottom-suit");
+  let cardValue = document.getElementById("card-value");
+
+  card.className = `card ${suitClass}`;
+  topSuit.textContent = suit;
+  bottomSuit.textContent = suit;
+  cardValue.textContent = value;
+}
+
+window.onload = () => {
+  generateCard();
+  setInterval(generateCard, 10000); 
 };
+
+document.getElementById("newCardBtn").addEventListener("click", generateCard);
+
